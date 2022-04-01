0f09bbef0adac5d07b4921ad437a5aaf73a9656d
JaumeKothan <jaumeollerfernandez@gmail.com>
2022-04-01 17:02:17 +0200

    web scrap 01/04

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from gettext import textdomain
 from bs4 import BeautifulSoup
 import requests
 
-text_html = requests.get("192.168.1.1:81").text
-print(text_html)
+text_html = requests.get("http://localhost:81/index_.php").text
+
+file = BeautifulSoup(text_html,"lxml")
+print(type(file))
