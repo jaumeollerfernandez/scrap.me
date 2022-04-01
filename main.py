5d41f7dde1880fccf5a92b114eea10875c7b4494
JaumeKothan <jaumeollerfernandez@gmail.com>
2022-04-01 13:37:33 +0200

    01/04 first test scraping beautifulsoup

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 from bs4 import BeautifulSoup
+
+
+with open("home.html", "r") as html_file:
+    #Lee el contenido html metiéndolo en una variable
+    content = html_file.read()
+
+    #Metemos la variable con el html, junto con el parser que utilizaremos
+    soup = BeautifulSoup(content, "lxml")
+
+    #Encuentra el primer elemento que especifiquemos, y luego para.
+    tags = soup.find("script")
+
+    #Encuentra todos los elementos que especifiquemos.
+    tags2 = soup.find_all("script")
+
+    courses_html_tags = soup.find_all("li")
+
+    #Una forma bastante limpia de obtener palabras concretas.
+    # for course in courses_html_tags:
+    #     print(course.text)
+
+    #Aquí podemos encontrar los precios del tag.
+    course_cards = soup.find_all("div",class_="caption")
+    for course in course_cards:
+        course_name = course.p.text
+        course_price = course.h4.text
+        print(course_name, course_price)
