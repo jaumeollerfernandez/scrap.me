7f80c67973f8816a87a6f2b0a9b950fdeca8a7ca
JaumeKothan <jaumeollerfernandez@gmail.com>
2022-04-22 17:01:56 +0200

    comparador realizado

---
+++ TratoFicheros.py
@@ -0,0 +1,46 @@
+from fileinput import close
+from msilib.schema import Class
+from pickle import NONE
+
+
+class TratoFicheros:
+    def __init__(self,p1=NONE,p2=NONE):
+        self.nombreFichero = p1
+        self.p2 = p2
+        
+    
+    def lectura(self):
+        fp = open(self.nombreFichero,'r')
+        datos = '.'
+        lista=[]
+        while datos != '':
+            datos=fp.readline()
+            if datos !='':
+                datos=datos.rstrip('\n')
+                dato = datos.split('*')
+                lista.append(dato)
+        fp.close()
+        return lista
+    
+    
+    
+    def escritura(self):
+        fp = open(self.nombreFichero,'w')
+        i=0
+        while i < len(self.p2):
+            j=0
+            while j < len(self.p2[i]):
+                fp.write(self.p2[i][j])
+                if j!=(len(self.p2[i])-1):
+                    fp.write('*')
+                j+=1
+            fp.write('\n')
+            i+=1
+        fp.close()
+        
+# lista=[['www.amazon.com','<div>','<div2>'],['www.amazon2.com','<div>','<div2>']]       
+
+
+# b = TratoFicheros(p1='archivo2.txt',p2=lista)
+# print(b.lectura())
+# b.escritura()
--- comparador.py
+++ comparador.py
@@ -4,40 +4,86 @@
 #Comparacion() deberá comparar los dos datos de los dos arrays. Leerá el anterior mediante el método, y luego lo comparará con el nuevo.
 #Si no hay novedad, mantendrá los precios actualizados.
 
-import writethings
+import TratoFicheros
+import datetime
 
 
-preciosactuales = ["Penetrator3000", "5,99","www.rabolargo.com/product/69"]
+m = [
+    ["Penetrator3000", "2.99","www.rabolargo.com/product/69"],
+    ["Penetrator4000","4.99","www.rabolargo.com/product/79"],
+    ["Penetrator5000","8.99","www.rabolargo.com/product/60"]
+    ]
 
 class ComparadorFinal():
     def __init__(self, preciosactuales):
         self.preciosactuales = preciosactuales
-
-    def FicheroExiste(self):
+    
+    def FicheroAlertaExiste(self):
+        #Lo unico que hace este metodo es crear el archivo si no existe.
         try:
             #Si existe, lo abre por aquí.
-            f = open("db.csv")
+            ale = open("alertas.txt")
         except IOError:
             #Si no existe, lo crea y lo escribe.
-            f = open("db.csv","w")
+            ale = open("alertas.txt","w")
+            print("Nuevo archivo de alertas creado")
+        finally:
+            ale.close()
+
+    def FicheroDBExiste(self):
+        try:
+            #Si existe, lo abre por aquí.
+            f = open("db.txt")
+        except IOError:
+            #Si no existe, lo crea y lo escribe.
+            f = open("db.txt","w")
             for i in range(len(self.preciosactuales)):
                 #Reemplazar por la clase escritura.
                 f.write(self.preciosactuales[i])
-                f.write(";")
+                f.write("*")
             print("Nuevo archivo creado")
         finally:
             f.close()
-    def comparacion(self):
-        #Aquí leerá el archivo db.csv con la clase escritura
-        oldfile = writethings.escritura("db.csv").leer()
-        print(oldfile)
+    def comparacion(self,fichero):
+        #Aquí leerá el archivo db con la clase escritura
+        oldfile = TratoFicheros.TratoFicheros(fichero).lectura()
+        resultado = []
+        #Empieza las comparaciones y búsquedas de precios
+        for i in range(len(oldfile)):
+            #Iteramos para buscar una coincidencia en el nombre
+            for j in range(len(oldfile[0])):
+                if oldfile[j][0] == self.preciosactuales[i][0]:
+                    #Convertimos a decimal
+                    a = float(self.preciosactuales[i][1])
+                    b = float(oldfile[j][1])
+                    #Comparamos precios. Si es menor, lo machaca.
+                    if a < b:
+                        oldfile[j][1] = str(a)
+                        #Realiza la alerta correspondiente y la escribe en el documento.
+                        self.FicheroAlertaExiste()
+                        time = str(datetime.datetime.now())
+                        with open ("alertas.txt","w") as f:
+                            f.write("El precio de ")
+                            f.write(oldfile[j][0])
+                            f.write(" es de ")
+                            f.write(str(a))
+                            f.write(" a las ")
+                            f.write(time)
+                            f.write("\n")
+        #Escribe los datos actualizados a la base de datos.                
+        TratoFicheros.TratoFicheros("db.txt",oldfile).escritura()
+        
+        return oldfile
+
+
+ComparadorFinal(m).comparacion("db.txt")
 
 
 
 
 
 
-ComparadorFinal(preciosactuales).comparacion()
+
 
 
 
