ede34558af3ebb9a79dfedf4e783b0c5bd63f6ca
JaumeKothan <jaumeollerfernandez@gmail.com>
2022-05-08 13:19:13 +0200

    08/05

--- interface/comparador_interface.py
+++ interface/comparador_interface.py
@@ -78,7 +78,7 @@ class Ui_MainWindow(object):
         self.alerta_name = QGroupBox(self.centralwidget)
         self.alerta_name.setObjectName(u"alerta_name")
         self.alerta_name.setGeometry(QRect(620, 20, 61, 16))
-        # MainWindow.setCentralWidget(self.centralwidget)
+        #MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         # MainWindow.setStatusBar(self.statusbar)
